af4bd8e728a72a0bac894e431b75f8d5c73066f7
tolujacob05 <tolujacob05@gmail.com>
2024-03-05 15:27:54 +0100

    some api implementation made

---
+++ blog/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/App.css";
+import Home from "./Home";
+import Logo from "./Logo";
+
+function App() {
+  return (
+    <>
+      <Logo />
+      <Home />
+    </>
+  );
+}
+
+export default App;
